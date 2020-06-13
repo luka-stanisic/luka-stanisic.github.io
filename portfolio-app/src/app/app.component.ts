cfc2f5e8ea5a6acbcf2fd832fe8b49739fca962b
Luka Stanisic <luka.stanisic@aol.com>
2020-06-13 18:23:43 +0200

    removed routing, added bootstrap

--- portfolio-app/src/app/app.component.ts
+++ portfolio-app/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio-app';
+
+  scrollToElement(element): void {
+    element.scrollIntoView({ behavior: 'smooth', inline: 'nearest' });
+  }
 }
--- portfolio-app/src/app/app.module.ts
+++ portfolio-app/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,16 +9,9 @@ import { FooterComponent } from './footer/footer.component';
 import { IconsModule } from './icons/icons.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ProjectsComponent,
-    HeaderComponent,
-    FooterComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, IconsModule],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent, AboutComponent, ProjectsComponent, HeaderComponent, FooterComponent],
+	imports: [BrowserModule, IconsModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
