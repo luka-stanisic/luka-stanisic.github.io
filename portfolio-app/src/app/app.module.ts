4243697395785f9579d94635ee72817c5fab7abc
Luka Stanisic <luka.stanisic@aol.com>
2020-06-27 21:31:08 +0200

    added components, created separate "under-maintenance" page

--- portfolio-app/src/app/app.module.ts
+++ portfolio-app/src/app/app.module.ts
@@ -7,9 +7,11 @@ import { ProjectsComponent } from './projects/projects.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { IconsModule } from './icons/icons.module';
+import { SkillsComponent } from './skills/skills.component';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
 
 @NgModule({
-	declarations: [AppComponent, AboutComponent, ProjectsComponent, HeaderComponent, FooterComponent],
+	declarations: [AppComponent, AboutComponent, ProjectsComponent, HeaderComponent, FooterComponent, SkillsComponent, MaintenanceComponent],
 	imports: [BrowserModule, IconsModule],
 	providers: [],
 	bootstrap: [AppComponent],
